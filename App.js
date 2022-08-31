e253743fee0171555b25c7baa2a9c8cabfc9a933
Saran barua <Saranbarua2357@gmail.com>
2022-08-31 12:04:20 +0600

    updated

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ export default function App() {
   return (
     <View style={[styles.container, {
       // Try setting `flexDirection` to `"row"`.
-      floxDirection: "column"
+      flexDirection: "row"
     }]}>
-      <View style={{ flex: 1, backgroundColor: "red" }} />
+      <View style={{ flex: 2, backgroundColor: "red" }} />
       <View style={{ flex: 2, backgroundColor: "darkorange" }} />
-      <View style={{ flex: 3, backgroundColor: "green" }} />
+      <View style={{ flex: 2, backgroundColor: "green" }} />
     </View>
   );
 }

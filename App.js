a4968174cc4a6f2f961af83e8f47673b1be15c14
Saran barua <Saranbarua2357@gmail.com>
2022-08-31 10:40:13 +0600

    updated

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello! This is my first React native App, please keep me in your prayers</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      floxDirection: "column"
+    }]}>
+      <View style={{ flex: 1, backgroundColor: "red" }} />
+      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 3, backgroundColor: "green" }} />
     </View>
   );
 }
 
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
